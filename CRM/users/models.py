ed793051fb25a4fd5dee84896126e80f667afae6
NikoTeLp90 <nikote.lp@gmail.com>
2024-04-20 22:48:09 -0300

    Styles and model user

--- CRM/users/models.py
+++ CRM/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class usuarios (models.Model):
+
+    username = models.CharField(max_length=50, required = True, unique= True)
+    nombre = models.CharField(max_length= 50, required = True)
+    apellido = models.CharField(max_length=50, required = True)
+    
--- CRM/users/views.py
+++ CRM/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, CreateView
 
 # Create your views here.
+
+
+
+class home(TemplateView):
+    template_name = "index.html"
+
+
+class createUser(CreateView):
+    model = MODEL_NAME
+    template_name = "TEMPLATE_NAME"
+
